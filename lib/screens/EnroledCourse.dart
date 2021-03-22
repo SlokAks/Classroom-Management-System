6b3aee52e00cea949ccfd119b79bd145dcdf015c
phantom654 <sumitkumarsahu01586@gmail.com>
2021-03-22 10:08:10 +0530

    added enrolled courses

--- lib/screens/EnroledCourse.dart
+++ lib/screens/EnroledCourse.dart
@@ -1,3 +1,4 @@
+import 'package:classroom_management/widgets/CustomListTile.dart';
 import 'package:classroom_management/widgets/appbar.dart';
 import 'package:classroom_management/widgets/navbar.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -24,7 +25,7 @@ class _EnroledCoursesState extends State<EnroledCourses> {
         stream: FirebaseFirestore.instance
             .collection('users')
             .doc(user.uid)
-            .collection('enrolled_courses')
+            .collection('enrolledCourses')
             .snapshots(),
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
           if (!snapshot.hasData) {
@@ -33,19 +34,34 @@ class _EnroledCoursesState extends State<EnroledCourses> {
             );
           }
           return ListView(
-            children: snapshot.data.docs.map((document) {
-              String courseTitle = "NA";
-              dbCourses
-                  .where("course_id", isEqualTo: document['course_id'])
-                  .get()
-                  .then((value) => value.docs
-                      .map((e) => {courseTitle = e.data()['course_name']}));
-              // return CustomListTile(
-              //   widget: Icon(Icons.book),
-              //   title: courseTitle,
-              //   description: "NA",
-              // );
-            }).toList(),
+            children: snapshot.data.docs.map(
+              (enrolledCoursesSnapshot) {
+                return FutureBuilder(
+                  future: FirebaseFirestore.instance
+                      .collection("Courses")
+                      .doc(enrolledCoursesSnapshot.id)
+                      .get(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<DocumentSnapshot> courseSnapsot) {
+                    if (courseSnapsot.hasError) {
+                      return Text("Something went wrong");
+                    }
+
+                    if (courseSnapsot.connectionState == ConnectionState.done) {
+                      return CustomListTile(
+                        enrolledCoursesSnapshot.id,
+                        title: courseSnapsot.data.data()["name"] +
+                            "(" +
+                            enrolledCoursesSnapshot.id +
+                            ")",
+                        description: courseSnapsot.data.data()["description"],
+                      );
+                    }
+                    return Text("loading");
+                  },
+                );
+              },
+            ).toList(),
           );
         },
       ),
