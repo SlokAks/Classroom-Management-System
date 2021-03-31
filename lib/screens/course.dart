a81d083d45e0334c31f7dcd7a47fbe0d08c5cd11
Pranav Singhal <iit2019050@iiita.ac.in>
2021-04-01 00:19:44 +0530

    Added Submitting Assignment for professor.(Add yourself as isProf:true in firebase to see UI.)

--- lib/screens/course.dart
+++ lib/screens/course.dart
@@ -1,8 +1,11 @@
+import 'package:classroom_management/screens/AddAssignment.dart';
 import 'package:classroom_management/screens/HomeAnnouncementsPage.dart';
 import 'package:classroom_management/screens/MakeAnnouncements.dart';
 import 'package:classroom_management/screens/assignments.dart';
 import 'package:classroom_management/screens/chat_screen.dart';
 import 'package:classroom_management/widgets/navbar.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Course extends StatefulWidget {
@@ -21,12 +24,28 @@ class _CourseState extends State<Course> {
   String courseId;
   String title = "NA";
   String description = "NA";
+  bool isLoading=true;
+  bool isProf=false;
   _CourseState({this.courseId, this.title, this.description});
+  findIfProfessor() async{
+    User user = FirebaseAuth.instance.currentUser;
+    DocumentSnapshot documentSnapshot=await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
+    setState(() {
+      isProf=documentSnapshot.data()['isProf'];
+      isLoading=false;
+    });
+
+  }
+  @override
+  void initState() {
+    findIfProfessor();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     final _kTabPages = <Widget>[
       Column(
-        children: [Announcements(courseId), MakeAnnouncements(courseId)],
+        children: [Announcements(courseId), if(isProf)MakeAnnouncements(courseId)],
       ),
       Assignments(
         courseId: courseId,
@@ -49,6 +68,28 @@ class _CourseState extends State<Course> {
         appBar: AppBar(
           title: Text(this.title),
           backgroundColor: Theme.of(context).accentColor,
+          actions: <Widget>[
+            if(isProf)
+            FlatButton(
+              textColor: Colors.white,
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => AddAssignment(courseId: widget.courseId,)));
+              },
+              child: Text("Add Assignment",style: TextStyle(
+                  fontSize: 22
+              ),),
+              shape: CircleBorder(side: BorderSide(color: Colors.transparent)),
+            ),
+            if(isProf)
+            FlatButton(
+              textColor: Colors.white,
+              onPressed: () {},
+              child: Text("Students",style: TextStyle(
+                fontSize: 22
+              ),),
+              shape: CircleBorder(side: BorderSide(color: Colors.transparent)),
+            ),
+          ],
           // If `TabController controller` is not provided, then a
           // DefaultTabController ancestor must be provided instead.
           // Another way is to use a self-defined controller, c.f. "Bottom tab
@@ -57,9 +98,16 @@ class _CourseState extends State<Course> {
             tabs: _kTabs,
           ),
         ),
-        body: TabBarView(
+        body: isLoading?Center(child: CircularProgressIndicator()):TabBarView(
           children: _kTabPages,
         ),
+        floatingActionButton: isProf?FloatingActionButton.extended(
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => AddAssignment(courseId: widget.courseId,)));
+          },
+          icon: Icon(Icons.add),
+          label: Text("Add Assignment"),
+        ): Container() ,
       ),
     );
   }
